d42cde87347ecdf007c54c941634b635dadfc673
Logan Dooley <logandooley123@gmail.com>
2026-09-18 12:06:03 -0400

    Add noise and thrust library

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,5 +1,28 @@
 #include "kernel.h"
 
+#include <thrust/execution_policy.h>
+#include <thrust/random.h>
+#include <thrust/remove.h>
+
+__host__ __device__ inline unsigned int utilhash(unsigned int a)
+{
+    a = (a + 0x7ed55d16) + (a << 12);
+    a = (a ^ 0xc761c23c) ^ (a >> 19);
+    a = (a + 0x165667b1) + (a << 5);
+    a = (a + 0xd3a2646c) ^ (a << 9);
+    a = (a + 0xfd7046c5) + (a << 3);
+    a = (a ^ 0xb55a4f09) ^ (a >> 16);
+    return a;
+}
+
+__host__ __device__
+thrust::default_random_engine makeSeededRandomEngine(int iter, int index, int depth)
+{
+    int h = utilhash((1 << 31) | (depth << 22) | iter) ^ utilhash(index);
+    return thrust::default_random_engine(h);
+}
+
+
 __host__ __device__ int divup(int a, int b) {
     return (a + b - 1) / b;
 }
@@ -75,9 +98,12 @@ __global__ void kernShade(PathState* dev_pathStates, IntersectionData* dev_inter
         return;
     }
 
+    thrust::default_random_engine rng = makeSeededRandomEngine(index, index, 0);
+    thrust::uniform_real_distribution<float> u01(0, 1);
+
     PathState pathState = dev_pathStates[index];
 
-    dev_pathStates[index].accumulatedColor = pathState.throughput * intersectionData.normal;
+    dev_pathStates[index].accumulatedColor = pathState.throughput * intersectionData.normal * u01(rng);
 }
 
 __global__ void kernColorSurface(cudaSurfaceObject_t surface, PathState* dev_pathStates, int n, int width)
